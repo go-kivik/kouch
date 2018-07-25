27d74be01705a36d0832e13b65e7b7e9d2cc0c9d
Jonathan Hall <flimzy@flimzy.com>
2018-07-25 15:41:08 +0200

    Fail when parent command isn't registered

--- cmd/registry/registry.go
+++ cmd/registry/registry.go
@@ -1,6 +1,8 @@
 package registry
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/spf13/cobra"
@@ -12,21 +14,59 @@ import (
 // CommandInitFunc returns a cobra sub command.
 type CommandInitFunc func(log log.Logger, conf *viper.Viper) *cobra.Command
 
+type subCommand struct {
+	children  map[string]*subCommand
+	initFuncs []CommandInitFunc
+}
+
 var initMU sync.Mutex
-var initFuncs []CommandInitFunc
+var rootCommand = newSubCommand()
+
+func newSubCommand() *subCommand {
+	return &subCommand{
+		children:  make(map[string]*subCommand),
+		initFuncs: []CommandInitFunc{},
+	}
+}
 
 // Register registers a sub-command.
 func Register(parent []string, fn CommandInitFunc) {
 	initMU.Lock()
 	defer initMU.Unlock()
-	initFuncs = append(initFuncs, fn)
+	cmd := rootCommand
+	for _, p := range parent {
+		if _, ok := cmd.children[p]; !ok {
+			cmd.children[p] = newSubCommand()
+		}
+		cmd = cmd.children[p]
+	}
+	cmd.initFuncs = append(cmd.initFuncs, fn)
 }
 
 // AddSubcommands initializes and adds all registered subcommands to cmd.
 func AddSubcommands(cmd *cobra.Command, l log.Logger, conf *viper.Viper) {
 	initMU.Lock()
 	defer initMU.Unlock()
-	for _, fn := range initFuncs {
-		cmd.AddCommand(fn(l, conf))
+	if err := addSubcommands(cmd, l, conf, nil, rootCommand); err != nil {
+		panic(err.Error())
 	}
+}
+
+func addSubcommands(cmd *cobra.Command, l log.Logger, conf *viper.Viper, path []string, cmdMap *subCommand) error {
+	children := make(map[string]*cobra.Command)
+	for _, fn := range cmdMap.initFuncs {
+		subCmd := fn(l, conf)
+		children[subCmd.Use] = subCmd
+		cmd.AddCommand(subCmd)
+	}
+	for name, childCmd := range cmdMap.children {
+		child, ok := children[name]
+		if !ok {
+			return fmt.Errorf("Subcommand '%s %s' not registered", strings.Join(path, " "), name)
+		}
+		if err := addSubcommands(child, l, conf, append(path, name), childCmd); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- cmd/registry/registry_test.go
+++ cmd/registry/registry_test.go
@@ -16,7 +16,7 @@ var registryLock sync.Mutex
 func lockRegistry() func() {
 	registryLock.Lock()
 	return func() {
-		initFuncs = []CommandInitFunc{}
+		rootCommand = newSubCommand()
 		registryLock.Unlock()
 	}
 }
@@ -26,54 +26,93 @@ func TestAddSubcommands(t *testing.T) {
 	initCount := 0
 	Register(nil, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
 		initCount++
-		return &cobra.Command{}
+		return &cobra.Command{Use: "foo"}
 	})
 	Register(nil, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
 		initCount++
-		return &cobra.Command{}
+		return &cobra.Command{Use: "bar"}
 	})
 	Register(nil, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
+		initCount++
+		return &cobra.Command{Use: "baz"}
+	})
+	Register([]string{"foo"}, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
 		initCount++
 		return &cobra.Command{}
 	})
 	AddSubcommands(&cobra.Command{}, nil, nil)
-	if initCount != 3 {
-		t.Errorf("Expected 3 initializations, got %d", initCount)
+	if expected := 4; initCount != expected {
+		t.Errorf("Expected %d initializations, got %d", expected, initCount)
+	}
+}
+
+func TestAddSubcommandsPanic(t *testing.T) {
+	defer lockRegistry()()
+	Register(nil, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
+		return &cobra.Command{Use: "foo"}
+	})
+	Register([]string{"foo", "bar", "baz"}, func(_ log.Logger, _ *viper.Viper) *cobra.Command {
+		return &cobra.Command{Use: "bar"}
+	})
+	recovered := func() (r interface{}) {
+		defer func() { r = recover() }()
+		AddSubcommands(&cobra.Command{}, nil, nil)
+		return nil
+	}()
+	expected := "Subcommand 'foo bar' not registered"
+	if d := diff.Interface(expected, recovered); d != nil {
+		t.Error(d)
 	}
 }
 
 func TestRegister(t *testing.T) {
+	nilFn := func(_ log.Logger, _ *viper.Viper) *cobra.Command {
+		return nil
+	}
 	type regTest struct {
-		name      string
-		parent    []string
-		fn        CommandInitFunc
-		expected  interface{}
-		recovered interface{}
+		name     string
+		init     func()
+		parent   []string
+		fn       CommandInitFunc
+		expected interface{}
 	}
 	tests := []regTest{
-		func() regTest {
-			fn := func(_ log.Logger, _ *viper.Viper) *cobra.Command {
-				return nil
-			}
-			return regTest{
-				name:     "simple",
-				fn:       fn,
-				expected: []CommandInitFunc{fn},
-			}
-		}(),
+		{
+			name: "simple",
+			fn:   nilFn,
+			expected: &subCommand{
+				children:  map[string]*subCommand{},
+				initFuncs: []CommandInitFunc{nilFn},
+			},
+		},
+		{
+			name:   "subcommand with no parent",
+			parent: []string{"foo", "bar"},
+			fn:     nilFn,
+			expected: &subCommand{
+				children: map[string]*subCommand{
+					"foo": &subCommand{
+						children: map[string]*subCommand{
+							"bar": &subCommand{
+								children:  map[string]*subCommand{},
+								initFuncs: []CommandInitFunc{nilFn},
+							},
+						},
+						initFuncs: []CommandInitFunc{},
+					},
+				},
+				initFuncs: []CommandInitFunc{},
+			},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			defer lockRegistry()()
-			recovered := func() (r interface{}) {
-				defer func() { r = recover() }()
-				Register(test.parent, test.fn)
-				return nil
-			}()
-			if d := diff.Interface(test.recovered, recovered); d != nil {
-				t.Error(d)
+			if test.init != nil {
+				test.init()
 			}
-			if d := diff.Interface(test.expected, initFuncs); d != nil {
+			Register(test.parent, test.fn)
+			if d := diff.Interface(test.expected, rootCommand); d != nil {
 				t.Error(d)
 			}
 		})
