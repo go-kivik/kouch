2fd0358702c84be4743494b201baf501bb967bc2
Jonathan Hall <flimzy@flimzy.com>
2018-07-28 17:36:48 +0200

    JSON mode instantiation

--- io/json.go
+++ io/json.go
@@ -7,18 +7,40 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const (
+	optJsonPrefix     = "json-prefix"
+	optJsonIndent     = "json-indent"
+	optJsonEscapeHTML = "json-escape-html"
+)
+
 type jsonMode struct{}
 
 var _ outputMode = &jsonMode{}
 
 func (m *jsonMode) config(cmd *cobra.Command) {
-	cmd.PersistentFlags().String("json-prefix", "", "Prefix to begin each line of the JSON output. See [https://golang.org/pkg/encoding/json/#Indent] for more information.")
-	cmd.PersistentFlags().String("json-indent", "", "Indentation string for JSON output. See [https://golang.org/pkg/encoding/json/#Indent] for more information.")
-	cmd.PersistentFlags().Bool("json-escape-html", false, "Enable escaping of special HTML characters. See [https://golang.org/pkg/encoding/json/#Encoder.SetEscapeHTML].")
+	cmd.PersistentFlags().String(optJsonPrefix, "", "Prefix to begin each line of the JSON output. See [https://golang.org/pkg/encoding/json/#Indent] for more information.")
+	cmd.PersistentFlags().String(optJsonIndent, "", "Indentation string for JSON output. See [https://golang.org/pkg/encoding/json/#Indent] for more information.")
+	cmd.PersistentFlags().Bool(optJsonEscapeHTML, false, "Enable escaping of special HTML characters. See [https://golang.org/pkg/encoding/json/#Encoder.SetEscapeHTML].")
 }
 
-func (m *jsonMode) new(cmd *cobra.Command) processor {
-	return &jsonProcessor{}
+func (m *jsonMode) new(cmd *cobra.Command) (processor, error) {
+	prefix, err := cmd.Flags().GetString(optJsonPrefix)
+	if err != nil {
+		return nil, err
+	}
+	indent, err := cmd.Flags().GetString(optJsonIndent)
+	if err != nil {
+		return nil, err
+	}
+	escapeHTML, err := cmd.Flags().GetBool(optJsonEscapeHTML)
+	if err != nil {
+		return nil, err
+	}
+	return &jsonProcessor{
+		prefix:     prefix,
+		indent:     indent,
+		escapeHTML: escapeHTML,
+	}, nil
 }
 
 type jsonProcessor struct {
--- io/json_test.go
+++ io/json_test.go
@@ -17,6 +17,64 @@ func TestJsonModeConfig(t *testing.T) {
 	testOptions(t, []string{"json-escape-html", "json-indent", "json-prefix"}, cmd)
 }
 
+func TestJsonNew(t *testing.T) {
+	tests := []struct {
+		name     string
+		args     []string
+		parseErr string
+		expected *jsonProcessor
+		err      string
+	}{
+		{
+			name:     "happy path, no options",
+			args:     nil,
+			expected: &jsonProcessor{},
+		},
+		{
+			name:     "invalid args",
+			args:     []string{"--foo"},
+			parseErr: "unknown flag: --foo",
+		},
+		{
+			name: "happy path, prefix",
+			args: []string{"--json-prefix", "xx"},
+			expected: &jsonProcessor{
+				prefix: "xx",
+			},
+		},
+		{
+			name: "happy path, indent",
+			args: []string{"--json-indent", "--"},
+			expected: &jsonProcessor{
+				indent: "--",
+			},
+		},
+		{
+			name: "happy path, escape html",
+			args: []string{"--json-escape-html"},
+			expected: &jsonProcessor{
+				escapeHTML: true,
+			},
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cmd := &cobra.Command{}
+			mode := &jsonMode{}
+			mode.config(cmd)
+
+			err := cmd.ParseFlags(test.args)
+			testy.Error(t, test.parseErr, err)
+
+			result, err := mode.new(cmd)
+			testy.Error(t, test.err, err)
+			if d := diff.Interface(test.expected, result); d != nil {
+				t.Error(d)
+			}
+		})
+	}
+}
+
 func TestJSONOutput(t *testing.T) {
 	tests := []struct {
 		name           string
