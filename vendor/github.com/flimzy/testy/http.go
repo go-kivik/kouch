2c04fd8ee7964bca4849716f85b9eb7661ddb916
Jonathan Hall <flimzy@flimzy.com>
2018-08-19 14:10:35 +0200

    Update deps

--- vendor/github.com/flimzy/testy/http.go
+++ vendor/github.com/flimzy/testy/http.go
@@ -1,10 +1,10 @@
 package testy
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"testing"
 )
 
 // ResponseHandler wraps an existing http.Response, to be served as a
@@ -35,24 +35,17 @@ func ServeResponse(r *http.Response) *httptest.Server {
 	return httptest.NewServer(&ResponseHandler{r})
 }
 
-// RequestValidator is a function that takes a *http.Request, and returns an
-// error if it does not meet expectations. The error is turned into a 400
-// response.
-type RequestValidator func(*http.Request) error
+// RequestValidator is a function that takes a *http.Request for validation.
+type RequestValidator func(*testing.T, *http.Request)
 
 // ValidateRequest returns a middleware that calls fn(), to validate the HTTP
 // request, before continuing. An error returned by fn() will result in the
 // addition of an X-Error header, a 400 status, and the error added to the
 // body of the response.
-func ValidateRequest(fn RequestValidator) func(http.Handler) http.Handler {
+func ValidateRequest(t *testing.T, fn RequestValidator) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if err := fn(r); err != nil {
-				w.Header().Add("X-Error", err.Error())
-				w.WriteHeader(http.StatusBadRequest)
-				fmt.Fprintf(w, "%s", err)
-				return
-			}
+			fn(t, r)
 			next.ServeHTTP(w, r)
 		})
 	}
@@ -60,7 +53,7 @@ func ValidateRequest(fn RequestValidator) func(http.Handler) http.Handler {
 
 // ServeResponseValidator wraps a ResponseHandler with ValidateRequest
 // middleware for a complete response-serving, request-validating test server.
-func ServeResponseValidator(r *http.Response, fn RequestValidator) *httptest.Server {
-	mw := ValidateRequest(fn)
+func ServeResponseValidator(t *testing.T, r *http.Response, fn RequestValidator) *httptest.Server {
+	mw := ValidateRequest(t, fn)
 	return httptest.NewServer(mw(&ResponseHandler{r}))
 }
