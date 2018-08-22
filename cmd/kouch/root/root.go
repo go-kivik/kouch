2e5746bd201bff5ebea3b4e55d11f7cb9b2fda0a
Jonathan Hall <flimzy@flimzy.com>
2018-08-22 12:47:36 +0200

    Move prerun to named func

--- cmd/kouch/root/root.go
+++ cmd/kouch/root/root.go
@@ -42,49 +42,12 @@ func Run() {
 // Run initializes the root command, adds subordinate commands, then executes.
 func rootCmd(version string) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           "kouch [options] [command] [target]",
-		Short:         "kouch is a command-line tool for interacting with CouchDB",
-		Version:       version,
-		SilenceUsage:  true,
-		SilenceErrors: true,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			ctx := kouch.GetContext(cmd)
-			ctx, err := setTarget(ctx, args)
-			if err != nil {
-				return err
-			}
-			if e := io.RedirStderr(cmd.Flags()); e != nil {
-				return e
-			}
-			ctx, err = verbose(ctx, cmd)
-			if err != nil {
-				return err
-			}
-			output, err := io.SelectOutput(cmd)
-			if err != nil {
-				return err
-			}
-			ctx = kouch.SetOutput(ctx, output)
-			outputer, err := io.SelectOutputProcessor(cmd)
-			if err != nil {
-				return err
-			}
-			ctx = kouch.SetOutputer(ctx, outputer)
-			conf, err := config.ReadConfig(cmd)
-			if err != nil {
-				return err
-			}
-			ctx = kouch.SetConf(ctx, conf)
-
-			input, err := io.SelectInput(cmd)
-			if err != nil {
-				return err
-			}
-			ctx = kouch.SetInput(ctx, input)
-
-			kouch.SetContext(ctx, cmd)
-			return nil
-		},
+		Use:               "kouch [options] [command] [target]",
+		Short:             "kouch is a command-line tool for interacting with CouchDB",
+		Version:           version,
+		SilenceUsage:      true,
+		SilenceErrors:     true,
+		PersistentPreRunE: prerun,
 	}
 
 	cmd.PersistentFlags().BoolP(flagVerbose, "v", false, "Make the operation more talkative")
@@ -94,6 +57,45 @@ func rootCmd(version string) *cobra.Command {
 
 	registry.AddSubcommands(cmd)
 	return cmd
+}
+
+func prerun(cmd *cobra.Command, args []string) error {
+	ctx := kouch.GetContext(cmd)
+	ctx, err := setTarget(ctx, args)
+	if err != nil {
+		return err
+	}
+	if e := io.RedirStderr(cmd.Flags()); e != nil {
+		return e
+	}
+	ctx, err = verbose(ctx, cmd)
+	if err != nil {
+		return err
+	}
+	output, err := io.SelectOutput(cmd)
+	if err != nil {
+		return err
+	}
+	ctx = kouch.SetOutput(ctx, output)
+	outputer, err := io.SelectOutputProcessor(cmd)
+	if err != nil {
+		return err
+	}
+	ctx = kouch.SetOutputer(ctx, outputer)
+	conf, err := config.ReadConfig(cmd)
+	if err != nil {
+		return err
+	}
+	ctx = kouch.SetConf(ctx, conf)
+
+	input, err := io.SelectInput(cmd)
+	if err != nil {
+		return err
+	}
+	ctx = kouch.SetInput(ctx, input)
+
+	kouch.SetContext(ctx, cmd)
+	return nil
 }
 
 func setTarget(ctx context.Context, args []string) (context.Context, error) {
