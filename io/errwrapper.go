e72efe4c9fd8e488cee2ed2e7bf03b48d3aff77f
Jonathan Hall <flimzy@flimzy.com>
2018-08-26 10:03:24 +0200

    Remove old outputer types

--- io/errwrapper.go
+++ io/errwrapper.go
@@ -4,19 +4,8 @@ import (
 	"io"
 
 	"github.com/go-kivik/couchdb/chttp"
-	"github.com/go-kivik/kouch"
 	"github.com/go-kivik/kouch/internal/errors"
 )
-
-type errWrapper struct {
-	kouch.OutputProcessor
-}
-
-var _ kouch.OutputProcessor = &errWrapper{}
-
-func (p *errWrapper) Output(w io.Writer, r io.ReadCloser) error {
-	return p.OutputProcessor.Output(&exitStatusWriter{w}, &exitStatusReadCloser{r})
-}
 
 type exitStatusWriter struct {
 	io.Writer
@@ -26,10 +15,7 @@ var _ io.Writer = &exitStatusWriter{}
 
 func (w *exitStatusWriter) Write(p []byte) (int, error) {
 	c, err := w.Writer.Write(p)
-	if err == nil {
-		return c, nil
-	}
-	return c, &errors.ExitError{Err: err, ExitCode: chttp.ExitWriteError}
+	return c, errors.WrapExitError(chttp.ExitWriteError, err)
 }
 
 type exitStatusReadCloser struct {
--- output.go
+++
@@ -1,8 +0,0 @@
-package kouch
-
-import "io"
-
-// OutputProcessor processes a command's output for display to a user.
-type OutputProcessor interface {
-	Output(io.Writer, io.ReadCloser) error
-}
