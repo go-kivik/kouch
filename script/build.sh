06a424adba638670a190e42a7f665e26dd16204b
Jonathan Hall <flimzy@flimzy.com>
2018-09-18 19:02:11 +0200

    Multi-arch build

--- script/build.sh
+++ script/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 set -e
 
-dir=$(go version | cut -d' ' -f4 | sed -e 's|/|-|')
-mkdir -p tmp/${dir}
-go build -o tmp/${dir}/kouch ./cmd/kouch
-cp LICENSE.md README.md tmp/${dir}
+targets="linux_386 linux_amd64 darwin_amd64 windows_386 windows_amd64"
 
 mkdir -p build
-tar -czvpf build/kouch-${TRAVIS_TAG}-${dir}.tar.gz tmp/${dir}
-zip -9r build/kouch-${TRAVIS_TAG}-${dir}.zip tmp/${dir}
+for target in $targets; do
+    echo Building ${target}...
+    mkdir -p tmp/${target}
+    bin=kouch
+    if [ "${target%_*}" == "windows" ]; then
+        bin=kouch.exe
+    fi
+    GOOS=${target%_*} GOARCH=${target##*_} go build -o tmp/${target}/${bin} ./cmd/kouch
+    cp LICENSE.md README.md tmp/${target}
+
+    tar -czvpf build/kouch-${TRAVIS_TAG/-/_}-${target}.tar.gz tmp/${target}
+    zip -9r build/kouch-${TRAVIS_TAG/-/_}-${target}.zip tmp/${target}
+done
